a249f420303b18b725584d7ac437b84429a7cd2c
Wastard <Ilya.pakylov@gmail.com>
2024-11-02 14:00:05 +0300

    added cart bill header

--- src/modules/menu/cart-bill/CartBill.tsx
+++ src/modules/menu/cart-bill/CartBill.tsx
@@ -2,8 +2,10 @@
 
 import { useCart } from '@/modules/cart/store'
 import Button from '@/shared/ui/button/Button'
+import { CategoryProp } from '@/shared/ui/category/Category.type'
 import CartList from '@/widgets/cart-list/CartList'
 import { CartPositionProp } from '@/widgets/cart-position/CartPosition.type'
+import Categories from '@/widgets/categories/Categories'
 import { useMenu } from '../store'
 import css from './CartBill.module.scss'
 
@@ -27,12 +29,21 @@ const CartBill = () => {
     return [...accum, cartPosition]
   }, [])
 
+  const lunchPlaces: CategoryProp[] = [{ text: 'Dine-in', active: true }, { text: 'Take away' }]
+
   const totalPrice = getTotalPrice()
 
   const totalItems = positions.reduce((accum, { amount }) => accum + amount, 0)
 
   return (
     <section className='flex flex-col h-full'>
+      <header className={css.header}>
+        <div className={css.title}>
+          <h1>Current Order</h1>
+          <strong>#90375</strong>
+        </div>
+        <Categories elements={lunchPlaces} />
+      </header>
       <div className='flex-1 px-6 overflow-auto'>
         <CartList elements={elements} />
       </div>
